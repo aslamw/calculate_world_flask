371b931420a2e4f07421ee2c9f4c53f61b6c467c
Marcos <wgngui14@gmail.com>
2023-04-14 01:17:07 -0300

    dockerfile4

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from src import app
+from waitress import serve
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.logger.info('Environment prod running. Port 5000')
+    serve(app, host="0.0.0.0", port=5000)
+    #app.run(debug=True, port=5000, host='0.0.0.0')
+    
